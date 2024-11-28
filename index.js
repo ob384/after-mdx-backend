9eefff67c0f29a80280959577d38419d18d8f453
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2024-11-28 22:14:49 +0100

    Static Images Using Path Join

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get("/api/search/courses", (req, res)=>{
 })
 
 app.get("/images/640x4:3", (req,res)=>{
-  res.sendFile(__dirname+"/public/images/placeholder.png")
+  path.join(__dirname, "public", "images", "placeholder.png")
 })
 
 app.post('/signup', (req, res)=>{
